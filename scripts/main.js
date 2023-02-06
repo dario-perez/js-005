68fe213b9562514ff27ac98aa893077dd02117e7
Dario Perez <darioperez.2105@gmail.com>
2023-02-06 17:19:57 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// ____________________________________Numbers
+
+const alturaCm = 172;
+const alturaM = 1.72;
+const pesoK = 124.5;
+const alturaR = Math.ceil(alturaM);
+const pesoR = Math.floor(pesoK);
+const maxValPlus = Number.MAX_VALUE + 1;
+
+if (maxValPlus === Number.MAX_VALUE) {
+  console.log(`Son iguales`);
+}
